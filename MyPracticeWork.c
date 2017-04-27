b5b74d6e5e17a90aebe3f3194ed4616ec22eaaa6
William Breen <wbreen@elon.edu>
2017-04-27 14:05:10 -0400

    finished practice question 2, reverse in place

--- MyPracticeWork.c
+++ MyPracticeWork.c
@@ -14,35 +14,41 @@ void stringCopy(char* in, char* out){
 // Question 2
 //reverse string in place
 void reverse(char* string, int size){
-	for(int i = 0;i<size; i++){
-		//how do you get the last char in a string?
-		//(*string) = 
+	char* end = string + size - 1;
+	while(string < end){
+		char temp = *string;
+		*string = *end;
+		*end = temp;
+		string++;
+		end--;
 	}
 }
 // Question 3
 // number of occurrences of a given letter in a given string
-int count(char* string, char letter){
+// int count(char* string, char letter){
 	
-}
+// 	return 0;
+// }
 
-// Question 4
-// print contents of an array of ints with tabs between elements
-void printArray(int array[], int size){
+// // Question 4
+// // print contents of an array of ints with tabs between elements
+// void printArray(int array[], int size){
 	
-}
+// }
 
-//Question 5
-// Sum the contents of an int array
-int addAll(int* array, int size){
+// //Question 5
+// // Sum the contents of an int array
+// int addAll(int* array, int size){
 	
-}
+// 	return 0;
+// }
 
-//Question 6
-//Create an array of ints in which the negative numbers come before the positive ones
-// (Note: You can't return an array in C. The output array must be an argument.)
-void negFirst(int array[], int size, int output[]){
+// //Question 6
+// //Create an array of ints in which the negative numbers come before the positive ones
+// // (Note: You can't return an array in C. The output array must be an argument.)
+// void negFirst(int array[], int size, int output[]){
 	
-}
+// }
 
 
 int main(void) {
@@ -51,33 +57,35 @@ int main(void) {
 	char* str2 = malloc(sizeof(char)*7); 
 	stringCopy(string, str2);
 	printf(str2);
+	printf("\n");
 	
 	// question 2 test code
 	//I have to declare it as an array or C will not let me modify the 
 	// String. 
-	char string[5] = "abcde";
-	reverse(string,5);
-	printf(string);
+	char chars[5] = "abcde";
+	reverse(chars,5);
+	printf(chars);
+	printf("\n");
 	
 	// Question 3 test code
-	char* trial1 = "help me please";
-	printf("%d",count(trial1,'e'));
+	// char* trial1 = "help me please";
+	// printf("%d",count(trial1,'e'));
 	// Should print 4.
 	
 	//Question 4 test code
-	int array[5] = {1,2,-3,4,-5};
-	printArray(array,5);
+	// int array[5] = {1,2,-3,4,-5};
+	// printArray(array,5);
 	
 	//Question 5 test code
-	int array[5] = {1,2,-3,4,-5};
-	printf("%d",addAll(array,5));
+	// int array[5] = {1,2,-3,4,-5};
+	// printf("%d",addAll(array,5));
 	
 	
 	//Question 6 test code
-	int array[5] = {1,2,-3,4,-5};
-	int* array2= malloc(sizeof(int)*5);
-	negFirst(array,5,array2);
-	printArray(array2,5);
+	// int array[5] = {1,2,-3,4,-5};
+	// int* array2= malloc(sizeof(int)*5);
+	// negFirst(array,5,array2);
+	// printArray(array2,5);
 	
 	return 0;
 }
